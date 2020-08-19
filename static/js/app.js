8ed2c3d9c21618f280afed98f2a9bb2209f5fb13
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-08-19 17:22:06 +0600

    adding navbar

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+    if($(this).scrollTop()>5) {
+        $( ".navbar-me" ).addClass("fixed-me");
+    } else {
+        $( ".navbar-me" ).removeClass("fixed-me");
+    }
+});
