4311c725e9c15baa67b80bd757717554ce40333d
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-08-20 20:09:46 +0600

    added user authentication properties

--- calories/urls.py
+++ calories/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView,RegisterPage,LoginPage
+from .views import HomePageView,RegisterPage,LoginPage,LogOutPage
 
 urlpatterns = [
 	path('', HomePageView,name='home'),
 	path('register/',RegisterPage,name='register'),
-	path('login/',LoginPage,name='login')
+	path('login/',LoginPage,name='login'),
+	path('logout/',LogOutPage,name='logout')
 ]
--- calories/views.py
+++ calories/views.py
@@ -2,33 +2,49 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 # Create your views here.
 
+@login_required(login_url='login')
 def HomePageView(request):
 	return render(request, 'home.html')
 
 def RegisterPage(request):
-	form = CreateUserForm()
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			form.save()
-			user = form.cleaned_data.get('username')
-			messages.success(request,user + "Your account was created")
-			return redirect('login')
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
 
-	context = {'form':form}
-	return render(request,'register.html',context)
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request,user + "Your account was created")
+				return redirect('login')
+
+		context = {'form':form}
+		return render(request,'register.html',context)
 
 
 def LoginPage(request):
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		user = authenticate(request,username=username,password=password)
-		if user is not None:
-			login(request,user)
-			return redirect('home')
-	context = {}
-	return render(request,'login.html',context)
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			user = authenticate(request,username=username,password=password)
+			if user is not None:
+				login(request,user)
+				return redirect('home')
+			else:
+				messages.info(request,'Username or password is incorrect')
+		context = {}
+		return render(request,'login.html',context)
+
+def LogOutPage(request):
+	logout(request)
+	return redirect('login')
