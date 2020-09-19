dae690f6636fb9bd6c11f8edbd8559b60dee29a8
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-09-19 14:39:27 +0600

    viewing past week record in profile page

--- calories/views.py
+++ calories/views.py
@@ -2,42 +2,52 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import SelectFoodForm,AddFoodForm,CreateUserForm
+from .forms import SelectFoodForm,AddFoodForm,CreateUserForm,ProfileForm
 from .models import *
 from datetime import timedelta
 from django.utils import timezone
 from datetime import date
+from datetime import datetime
 from .filters import FoodFilter
 # Create your views here.
 
 @login_required(login_url='login')
 def HomePageView(request):
 	
+	
+	calories = Profile.objects.filter(person_of=request.user).last()
+
+	calorie_goal = calories.calorie_goal
+	
+	if date.today() > calories.date:
+		profile=Profile.objects.create(person_of=request.user)
+		
 	calories = Profile.objects.filter(person_of=request.user).last()
 	print(calories.id)
 	print(calories.date)
-	calorie_goal = calories.calorie_goal
-	if date.today() > calories.date:
-		profile=Profile.objects.create(person_of=request.user)
-	some_day_last_week = timezone.now().date() - timedelta(days=7)
-	monday_of_last_week = some_day_last_week - timedelta(days=(some_day_last_week.isocalendar()[2] - 1))
-	monday_of_this_week = monday_of_last_week + timedelta(days=7)
-	records=Profile.objects.filter(date__gte=monday_of_last_week, date__lt=monday_of_this_week,person_of=request.user)
-	print(records)
+	
+	
+	
 
 	calorie_goal_status = calorie_goal -calories.total_calorie
 	over_calorie = 0
 	if calorie_goal_status < 0 :
 		over_calorie = abs(calorie_goal_status)
 
+	
+	food_selected_today = calories.food_selected_today
+	
+
 	context = {
 	'total_calorie':calories.total_calorie,
-	'records':records,
+	
 	'calorie_goal':calorie_goal,
 	'calorie_goal_status':calorie_goal_status,
-	'over_calorie' : over_calorie
+	'over_calorie' : over_calorie,
+	'food_selected_today':food_selected_today
 	}
 	
+
 	return render(request, 'home.html',context)
 
 def RegisterPage(request):
@@ -118,3 +128,30 @@ def add_food(request):
 
 	return render(request,'add_food.html',context)
 
+@login_required
+def ProfilePage(request):
+	person = Profile.objects.filter(person_of=request.user).last()
+	food_items = Food.objects.filter(person_of=request.user)
+	form = ProfileForm(instance=person)
+
+	if request.method == 'POST':
+		form = ProfileForm(request.POST,instance=person)
+		if form.is_valid():
+			
+			form.save()
+			return redirect('profile')
+	else:
+		form = ProfileForm(instance=person)
+
+
+	some_day_last_week = timezone.now().date() -timedelta(days=7)
+	
+	
+	records=Profile.objects.filter(date__gte=some_day_last_week,date__lt=timezone.now().date(),person_of=request.user)
+	
+	print(records[1].calorie_goal)
+
+
+	context = {'form':form,'food_items':food_items,'records':records}
+
+	return render(request, 'profile.html',context)
