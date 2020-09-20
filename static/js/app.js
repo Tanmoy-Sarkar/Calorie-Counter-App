a8a3c00a489a2eed98923363842c91a4dfd597f8
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-09-20 19:58:23 +0600

    changing some bugs

--- static/js/app.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-    if($(this).scrollTop()>5) {
-        $( ".navbar-me" ).addClass("fixed-me");
-    } else {
-        $( ".navbar-me" ).removeClass("fixed-me");
-    }
-});
