fc51c387eaf555c353e43db84ad2758565767526
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-09-20 21:59:01 +0600

    j

--- calorie_app/settings.py
+++ calorie_app/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'calories.apps.CaloriesConfig',
+    'django_filters'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',  
-    'calories.apps.CaloriesConfig',
-    'django_filters'
+    
 ]
 
 MIDDLEWARE = [
